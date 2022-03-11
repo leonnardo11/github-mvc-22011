4d826cd47d4ffd60187570b8ecd43b3b2bbdbef9
Leonardo Rodrigues <54442873+leonnardo11@users.noreply.github.com>
2022-03-11 19:38:17 -0300

    corrigido?

---
+++ app/Models/ProdutosVenda.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProdutosVenda extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['id', 'venda_id', 'prodtudo_id', 'quantidade', 'valor'];
+    protected $table = 'ProdutosVenda';
+
+
+}
