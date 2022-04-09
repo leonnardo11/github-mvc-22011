e6c210304ab1d400c8e09bfb6960f394263d7aa6
Leonardo Rodrigues <54442873+leonnardo11@users.noreply.github.com>
2022-04-08 22:12:19 -0300

    Create SeederUserAdmin.php

---
+++ primeiro_site/database/seeders/SeederUserAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class SeederUserAdmin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([ // create user admin
+            'name' => 'Leonardo',
+            'email' => 'leonardo@leonardo.com',
+            'password' => bcrypt('123456')]);
+
+        $role = Role::create(['name' => 'Admin']); // create a new role called admin
+        $permissions = Permission::pluck('id','id')->all(); // get all permissions id
+        $role->syncPermissions($permissions); // assign all permissions to admin role
+        $user->assignRole([$role->id]); // assign role to user
+
+    }
+}
