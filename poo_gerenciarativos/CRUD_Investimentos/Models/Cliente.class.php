07cfea38c5adc00af12788965b92251ad014c5ab
Leonardo Rodrigues <54442873+leonnardo11@users.noreply.github.com>
2022-04-26 20:24:26 -0300

    foi

---
+++ poo_gerenciarativos/CRUD_Investimentos/Models/Cliente.class.php
@@ -0,0 +1,35 @@
+<?php 
+
+require __DIR__ . '/Model.class.php';
+
+class Cliente extends Model{
+    public function __construct(){
+        parent::__construct(); // chama o construtor da classe pai
+        $this->tabela = 'clientes';
+
+    }
+    function inserir(array $dados):?int{
+        $stmt = $this->pdo->prepare('INSERT INTO {$this->tabela} (nome, telefone) VALUES (:nome, :telefone)');
+        $stmt->bindValue(':nome', $dados['nome']);
+        $stmt->bindValue(':telefone', $dados['telefone']);
+
+          if($stmt->execute()){
+               echo $this->lastInsertId();
+         }        
+
+        return null;
+     }
+     function atualizar(int $id, array $dados):bool{
+        return true;
+     }
+     function apagar(int $id):bool{
+        return true;
+     }
+     function listar(int $id = null):?array{
+        return null;
+     }
+}
+
+$cliente = new Cliente();
+
+$cliente->inserir(['nome' => 'João', 'telefone' => '9999-9999']);
---
+++ poo_gerenciarativos/CRUD_Investimentos/Models/Model.class.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+
+abstract class Model extends PDO {
+    protected string $tabela;
+    public function __construct(){
+        if(!defined(DSN) || !defined(DB_USER) || !defined(DB_PASS)){
+            throw new Exception('Configurações de banco de dados não encontradas');
+        }
+        parent::__construct(DSN, DB_USER, DB_PASS);
+    }
+    abstract function inserir(array $dados):?int;
+     abstract function atualizar(int $id, array $dados):bool;
+    abstract function apagar(int $id):bool;
+    abstract function listar(int $id = null):?array;
+}
